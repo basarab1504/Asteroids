e101ccb8e18bceb681995fac54f2e68a4cb354b2
basarab1504 <45500276+basarab1504@users.noreply.github.com>
2020-10-24 23:08:28 +0300

    clamper fix

--- Assets/Scripts/AreaClamper.cs
+++ Assets/Scripts/AreaClamper.cs
@@ -6,6 +6,16 @@ public class AreaClamper : MonoBehaviour
 {
     void OnTriggerExit2D(Collider2D other)
     {
-        transform.position = -transform.position;
+        float x = transform.position.x;
+        float y = transform.position.y;
+
+        Vector3 pos;
+
+        if (Mathf.Abs(x) >= Mathf.Abs(y))
+            pos = new Vector3(-x, y);
+        else
+            pos = new Vector3(x, -y);
+
+        transform.position = pos;
     }
 }
